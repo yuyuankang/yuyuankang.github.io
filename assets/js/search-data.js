f1d0c758ee68f0f72f1c791b65e41436a9a80616
yuyuankang <36235611+yuyuankang@users.noreply.github.com>
2025-09-18 06:40:37 +0000

    Deploying to gh-pages from @ yuyuankang/yuyuankang.github.io@581466dc686e4dd9e526249e3c3aec8d6186b8a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "About",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "Publications",
+          title: "publications",
           description: "",
           section: "Navigation",
           handler: () => {
